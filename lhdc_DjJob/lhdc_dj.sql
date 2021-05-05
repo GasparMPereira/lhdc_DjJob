3c0313aa89b51b5f5d2f9018ee17342b3406d97b
Gasparpereira2004 <71574610+Gasparpereira2004@users.noreply.github.com>
2021-05-05 11:22:12 +0100

    Add files via upload

---
+++ lhdc_DjJob/lhdc_dj.sql
@@ -0,0 +1,9 @@
+INSERT INTO `jobs` (name, label) VALUES
+  ('clubdj', 'Club DJ')
+;
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+('clubdj', 0, 'clubdj', 'Club DJ', 0, '{}', '{}')
+;
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+('dj_tip', 'DJ Tips', 15, 0, 1)
+;
