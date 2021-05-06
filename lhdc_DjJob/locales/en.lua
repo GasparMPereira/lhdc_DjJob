a245355d030091c0c3719565ac7cead98837398e
Gaspar <71574610+Gasparpereira2004@users.noreply.github.com>
2021-05-06 22:01:13 +0100

    Update Language file

--- lhdc_DjJob/locales/en.lua
+++ lhdc_DjJob/locales/en.lua
@@ -1,18 +1,15 @@
+--Name EN but language in Portuguese(PT)--
 Locales['en'] = {
-  -- Global menus
   ['cloakroom'] = 'DJ Menu',
-  ['cloak_change'] = 'press ~INPUT_PICKUP~ for DJ menu.',
+  ['cloak_change'] = 'Pressiona ~INPUT_PICKUP~ para o menu.',
   ['stop_work'] = 'Parar de trabalhar',
   ['start_work'] = 'Começar a fazer Mix`s',
-  ['foot_work']                 = 'you have to be on foot to be able to work.',
-  ['max_limit']                 = 'you cannot carry more ~y~%s~s~',
-  ['not_enough']                = 'Não tens ~y~%s~s~ para trocar.',
-  ['delivery_point']            = 'delivery point',
-  ['delivery']                  = 'delivery',
-
-  -- clubdj
+  ['foot_work'] = 'Tens de estar a pé para trabalhares.',
+  ['max_limit'] = 'Não podes carregar ~y~%s~s~',
+  ['not_enough'] = 'Não tens ~y~%s~s~ para trocar.',
+  ['delivery'] = 'Entrega/Troca',
   ['dj_club_locker'] = 'DJ Job Menu',
-  ['dj_play_music'] = 'press ~INPUT_PICKUP~ to start paying music.',
+  ['dj_play_music'] = 'Pressiona ~INPUT_PICKUP~ para trabalhares.',
   ['dj_tip'] = 'DJ Tips',
   ['dj_cash_tips'] = 'Pressiona ~INPUT_PICKUP~ para trocar as tips.',
 }
